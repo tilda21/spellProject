c381b362e840a04a2c9c7e794c59480beeb10a76
nuno <ngplbc@gmail.com>
2019-11-11 12:36:54 +0000

    Fix proxy and db url for local use

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const path = require("path");
 
 const leaderboardRouter = require("./routers/leaderboardRouter");
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const dbUrl = process.env.MONGODB_URI || process.env.DB_URL;
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to DB"))
   .catch(e => console.log(e.message));
 
